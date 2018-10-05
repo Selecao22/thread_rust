0c6e3f3f5aafe6c80e1ebdb22c4ac422832f54d9
Yazykov Nikita <cour1337@gmail.com>
2018-10-05 11:43:00 +0300

    added constants and formattig output

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::thread;
 use std::time::{Duration, Instant};
 use std::sync::{Arc, Mutex};
 
+const MAX_TIME: u64 = 6;
+const MAX_VALUE: u64 = 12;
+
 fn main() {
 
     let mut handles = vec![];
@@ -13,15 +16,15 @@ fn main() {
     for i in 0..5
     {
         let mux = Arc::clone(&mx);
-        let time = rand::random::<u64>() % 6;
+        let time = rand::random::<u64>() % MAX_TIME;
         let tt = i;
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_secs(time));
             let mut num = mux.lock().unwrap();
             println!("Поток № {}", tt);
             println!("Спал {} с", time);
-            *num = rand::random::<u64>() % 12;
-            println!("Значение, которое утановил поток {}", *num);
+            *num = rand::random::<u64>() % MAX_VALUE;
+            println!("Значение, которое утановил поток {}\n", *num);
             });
 
         handles.push(handle);
@@ -35,5 +38,5 @@ fn main() {
 
     thread::sleep(Duration::from_secs(6));
     println!("Последнее значение в мьютексе {}", *mx.lock().unwrap());
-    println!("Программа выполнилась за: {} с", Instant::now().duration_since(start_time).as_secs());
+    println!("Программа выполнилась за {} с", Instant::now().duration_since(start_time).as_secs());
 }
